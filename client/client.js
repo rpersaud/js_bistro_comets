196b9bab80c83efd5c1d1096233e076c5a24f541
Alicia Persaud <alicia@localhost.localdomain>
2013-03-09 03:46:18 +0100

    added pending labels

--- client/client.js
+++ client/client.js
@@ -8,18 +8,11 @@ Template.form.events({
       "name" : name,
       "phone" : phone,
       "number" : num,
-      "status" : 1
+      "status" : 0
     });
   }
 });
 
-
-
-/*
- <form>
- <input type="text" name="name" placeholder="Name" />
- <input type="tel" name="phone" placeholder="Phone Number" />
- <input type="text" name="number" placeholder="Number in Party" />
- <button type="submit" class="button radius medium eight columns offset-by-four">Add Reservation</button>
- </form>
- */
+Template.pending.reservations = function() {
+  return Reservations.find({status:0});
+}
